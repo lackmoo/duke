8e2af3e8244ee80aa2f083ee1a00a77dfd8a0abd
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2019-08-18 14:59:34 +0800

    Added the printLine() function to print the horizontal line

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,51 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
+    public static void printLine() {
+        String line = "\t____________________________________________________";
+        System.out.println(line);
+    }
     public static void main(String[] args) {
-        String logo = " ____ _ \n"
-                + "| _ \\ _ _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| | < __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke. What can I do for you?");
-        System.out.println("________________________________________");
+        String logo = " \t____ _ \n"
+                + "\t| _ \\ _ _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| | < __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("\tHello from\n" + logo);
+        printLine();
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        printLine();
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         String input = scanner.nextLine();
         /*while (!input.equals("bye")) {
-            System.out.println("________________________________________");
+            printLine();
             System.out.println(input);
-            System.out.println("________________________________________");
+            printLine();
             input = scanner.nextLine();
           }
-            System.out.println("________________________________________");
+            printLine();
             System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("________________________________________");*/
+            printLine();
+         */
         while (!input.equals("bye")) {
             if (!input.equals("list")) {
                 list.add(input);
-                System.out.println("________________________________________");
-                System.out.println("added: " + input);
-                System.out.println("________________________________________");
+                printLine();
+                System.out.println("\tadded: " + input);
+                printLine();
             } else {
+                printLine();
                 for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i-1));
+                    System.out.println("\t" + i + ". " + list.get(i-1));
                 }
-                System.out.println("________________________________________");
+                printLine();
             }
             input = scanner.nextLine();
         }
-        System.out.println("________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________");
+        printLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printLine();
     }
 }
