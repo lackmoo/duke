bb41975f4be0d07cfb9abb811774d93f73def3ca
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2019-08-18 12:12:43 +0800

    Level-1:Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke. What can I do for you?");
+        System.out.println("________________________________________");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        //System.out.println("________________________________________");
-        System.out.println("Hello! I'm Duke. What can I do for you?");
         while (!input.equals("bye")) {
             System.out.println("________________________________________");
             System.out.println(input);
