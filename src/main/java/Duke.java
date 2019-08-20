4e4249099129e8231bc2b281a4e8063aed5f0a17
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2019-08-20 22:29:24 +0800

    Made some edits for Level-2:Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class Duke {
     public static void printLine() {
         String line = "\t____________________________________________________";
         System.out.println(line);
     }
     public static void main(String[] args) {
-        String logo = " \t____ _ \n"
-                + "\t| _ \\ _ _| | _____ \n"
+        String logo = " \t____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
                 + "\t| | | | | | | |/ / _ \\\n"
-                + "\t| |_| | |_| | < __/\n"
+                + "\t| |_| | |_| |   <  __/\n"
                 + "\t|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("\tHello from\n" + logo);
         printLine();
         System.out.println("\tHello! I'm Duke");
@@ -19,16 +21,6 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         String input = scanner.nextLine();
-        /*while (!input.equals("bye")) {
-            printLine();
-            System.out.println(input);
-            printLine();
-            input = scanner.nextLine();
-          }
-            printLine();
-            System.out.println("Bye. Hope to see you again soon!");
-            printLine();
-         */
         while (!input.equals("bye")) {
             if (!input.equals("list")) {
                 list.add(input);
